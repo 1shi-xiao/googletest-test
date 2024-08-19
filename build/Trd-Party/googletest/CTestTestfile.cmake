d48d1da610b49e695b1e227b787a4987cd0a7883
ashi_xiao <ashi_xiao@163.com>
2024-08-19 20:19:42 +0800

    update .gitignore file

--- build/Trd-Party/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/googletest/GQITest/Trd-Party/googletest
-# Build directory: D:/googletest/GQITest/build/Trd-Party/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/Trd-Party/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/googletest/GQITest/Trd-Party/googletest/googletest
-# Build directory: D:/googletest/GQITest/build/Trd-Party/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
